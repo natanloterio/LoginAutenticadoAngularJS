684e25ded501fb2304d28632d45930042247f786
natanloterio <natanloterio@gmail.com>
2014-07-23 19:59:03 -0300

    Preparando pacote boewer

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,10 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-
+    $scope.alerta = function(){
+        alert('Alerta chamado');
+    }
+    $scope.pessoa = {nome:"",sobrenome:""};
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
